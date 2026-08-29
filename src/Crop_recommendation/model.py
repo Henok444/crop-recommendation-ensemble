fc864bb7c97a1a4c9bb87a7c611714f5bde85d32
henok444 <novelopp@gmail.com>
2026-08-29 21:11:14 +0300

    feat: add random forest model setup module

---
+++ src/Crop_recommendation/model.py
@@ -0,0 +1,11 @@
+from sklearn.ensemble import RandomForestClassifier
+
+
+
+def train_random_forest(X_train , y_train , n_estimator  , max_depth , random_state   ):
+    model = RandomForestClassifier(n_estimators= n_estimator ,
+                                   max_depth=max_depth ,
+                                   random_state=random_state , 
+                                   n_jobs = -1)
+    model.fit(X_train , y_train)
+    return model
