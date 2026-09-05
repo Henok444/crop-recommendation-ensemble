1823c50872161375b132efd4b0d654edc6cbbf6e
henok444 <novelopp@gmail.com>
2026-09-05 12:25:27 +0300

    feat: decode label indices to original class names post-prediction

--- src/Crop_recommendation/prediction.py
+++ src/Crop_recommendation/prediction.py
@@ -1,6 +1,9 @@
 import pandas as pd 
 from src.Crop_recommendation.validation import validate_features
+from src.Crop_recommendation.config import load_config
+from src.Crop_recommendation.model_io import load_encoder
 
+config = load_config()
 
 column_order = [
     "N",
@@ -17,6 +20,8 @@ def predict(model , features ):
     validate_features(features)
 
     x = pd.DataFrame([features] , columns= column_order)
-    y = model.predict(x)
-    return y[0]
+    y_numeric = model.predict(x)
+    lr = load_encoder(config["encoder"]["path"])
+    y_text = lr.inverse_transform(y_numeric)
+    return y_text[0]
 
