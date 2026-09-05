0ea41f67a3ae9c50a2ef92a465cf5cefa72f9061
henok444 <novelopp@gmail.com>
2026-09-05 12:22:15 +0300

    feat: add lable encoder saving and loading script

--- src/Crop_recommendation/model_io.py
+++ src/Crop_recommendation/model_io.py
@@ -7,3 +7,12 @@ def save_model(model , path: Path ):
 
 def load_model(path : Path ):
     return joblib.load(path)
+
+# for the encoder part 
+
+def save_encoded_label(lr , path: Path):
+
+    path.parent.mkdir(parents = True , exist_ok = True )
+    joblib.dump(lr , path)
+def load_encoder(path: Path):
+    return joblib.load(path)
